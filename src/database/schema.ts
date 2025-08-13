49108b409e1fbf88a0edb469a3f9c85263516068
Gustavo Guilherme <guguiantunes@hotmail.com>
2025-08-13 09:24:59 -0400

    adiciona tabela "courses" e atualiza o esquema do banco de dados

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -4,4 +4,10 @@ export const users = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   email: text().notNull().unique(),
-})
+});
+
+export const courses = pgTable("courses", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  description: text().notNull(),
+});
