bbc6e2126f9d95ee7c99b0b0a984e8e339e52957
Gustavo Guilherme <guguiantunes@hotmail.com>
2025-08-14 12:54:31 -0400

    Adiciona tabela de "enrollments" ao esquema do banco de dados, incluindo colunas e restrições de chave estrangeira. Atualiza o arquivo de migração e o snapshot correspondente. Adiciona script de semeadura para popular dados de usuários, cursos e matrículas utilizando a biblioteca @faker-js/faker.

---
+++ src/database/seed.ts
@@ -0,0 +1,33 @@
+import { fakerPT_BR } from '@faker-js/faker'
+import { db } from './client.ts'
+import { courses, enrollments, users } from './schema.ts'
+
+const seedUsers = [
+  { name: fakerPT_BR.person.fullName(), email: fakerPT_BR.internet.email() },
+  { name: fakerPT_BR.person.fullName(), email: fakerPT_BR.internet.email() },
+  { name: fakerPT_BR.person.fullName(), email: fakerPT_BR.internet.email() },
+  { name: fakerPT_BR.person.fullName(), email: fakerPT_BR.internet.email() },
+]
+
+const seedCourses = [
+  { title: fakerPT_BR.lorem.words(5), description: fakerPT_BR.lorem.words(5) },
+  { title: fakerPT_BR.lorem.words(5), description: fakerPT_BR.lorem.words(5) },
+  { title: fakerPT_BR.lorem.words(5), description: fakerPT_BR.lorem.words(5) },
+]
+
+async function seed() {
+  const usesrsInsert = await db.insert(users).values(seedUsers).returning()
+  const coursesInsert = await db.insert(courses).values(seedCourses).returning()
+
+  if (!coursesInsert[0] || !usesrsInsert[0]) {
+    throw new Error('Failed to insert seed data')
+  }
+
+  await db.insert(enrollments).values([
+    { courseId: coursesInsert[0].id, userId: usesrsInsert[0].id },
+    { courseId: coursesInsert[0].id, userId: usesrsInsert[0].id },
+    { courseId: coursesInsert[0].id, userId: usesrsInsert[0].id },
+  ])
+}
+
+seed()
